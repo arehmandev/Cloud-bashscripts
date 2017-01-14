b3ce14626ab14ba1046e63781a02aaa4ff342134
Abdul Rehman <arehmandev@outlook.com>
2017-01-14 17:21:55 +0000

    Update asgipcompare.sh

--- asgipcompare.sh
+++ asgipcompare.sh
@@ -3,11 +3,6 @@
 EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 
-
-function route53 {
-docker run garland/aws-cli-docker aws route53 list-resource-record-sets --hosted-zone-id $1 | jq .ResourceRecordSets[4].ResourceRecords[0,1,2].Value
-}
-
 function getip {
 for i in `docker run garland/aws-cli-docker aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $1 --region=$EC2_REGION | grep -i instanceid  | awk '{ print $2}' | cut -d',' -f1| sed -e 's/"//g'`
 do
@@ -15,13 +10,20 @@ docker run garland/aws-cli-docker aws ec2 describe-instances --instance-ids $i -
 done;
 }
 
-asgips=$(getip | sort -V)
-route53ips=$(route53 | tr -d "\"" | sort -V)
+function route53 {
+docker run garland/aws-cli-docker aws route53 list-resource-record-sets --hosted-zone-id $1 | jq .ResourceRecordSets[4].ResourceRecords[0,1,2].Value
+}
 
-if [[ $asgips != $route53ips ]]; then
-  echo "IPs are invalid"
-else
-  echo "IPs are the same no changes needed"
-fi
+hostedzone=$2
+asgname=$1
 
-# Give the hosted zone as an argument e.g. Z4LXVANPVC5RF
+asgips=$(getip $asgname | sort -V)
+route53ips=$(route53 $hostedzone | tr -d "\"" | sort -V)
+
+echo $asgips
+echo ---
+echo ---
+echo $route53ips
+
+# Give the ASG name as $1 argument
+# Give the hosted zone as an $2 argument e.g. Z4LXVANPVC5RF
